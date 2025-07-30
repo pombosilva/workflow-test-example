11e882ea09a577389e002d69bb344a0e83e2a0a5
Sid Chatterjee <sid@cloudflare.com>
2025-07-30 16:02:41 +0100

    update workflow

--- src/index.ts
+++ src/index.ts
@@ -25,37 +25,37 @@ export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
     // Can access bindings on `this.env`
     // Can access params on `event.payload`
 
-    const files = await step.do("my first step", async () => {
-      // Fetch a list of files from $SOME_SERVICE
-      return {
-        inputParams: event,
-        files: [
-          "doc_7392_rev3.pdf",
-          "report_x29_final.pdf",
-          "memo_2024_05_12.pdf",
-          "file_089_update.pdf",
-          "proj_alpha_v2.pdf",
-          "data_analysis_q2.pdf",
-          "notes_meeting_52.pdf",
-          "summary_fy24_draft.pdf",
-        ],
-      };
-    });
+    // const files = await step.do("my first step", async () => {
+    //   // Fetch a list of files from $SOME_SERVICE
+    //   return {
+    //     inputParams: event,
+    //     files: [
+    //       "doc_7392_rev3.pdf",
+    //       "report_x29_final.pdf",
+    //       "memo_2024_05_12.pdf",
+    //       "file_089_update.pdf",
+    //       "proj_alpha_v2.pdf",
+    //       "data_analysis_q2.pdf",
+    //       "notes_meeting_52.pdf",
+    //       "summary_fy24_draft.pdf",
+    //     ],
+    //   };
+    // });
 
     // You can optionally have a Workflow wait for additional data,
     // human approval or an external webhook or HTTP request, before progressing.
     // You can submit data via HTTP POST to /accounts/{account_id}/workflows/{workflow_name}/instances/{instance_id}/events/{eventName}
-    const waitForApproval = await step.waitForEvent("request-approval", {
-      type: "approval", // define an optional key to switch on
-      timeout: "1 minute", // keep it short for the example!
-    });
+    // const waitForApproval = await step.waitForEvent("request-approval", {
+    //   type: "approval", // define an optional key to switch on
+    //   timeout: "1 minute", // keep it short for the example!
+    // });
 
-    const apiResponse = await step.do("some other step", async () => {
-      let resp = await fetch("https://api.cloudflare.com/client/v4/ips");
-      return await resp.json<any>();
-    });
+    // const apiResponse = await step.do("some other step", async () => {
+    //   let resp = await fetch("https://api.cloudflare.com/client/v4/ips");
+    //   return await resp.json<any>();
+    // });
 
-    await step.sleep("wait on something", "1 minute");
+    await step.sleep("wait on something", "3 seconds");
   }
 }
 export default {
